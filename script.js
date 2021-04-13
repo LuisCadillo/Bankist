e113d6eb93ecdba4509e06fbd21a31014f4e7ab2
LuisCadillo <luiscaf3r@gmail.com>
2021-04-13 12:30:20 -0500

    basic function, display user 1  movements

--- script.js
+++ script.js
@@ -67,3 +67,17 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const addMovements = function(movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+    `
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+addMovements(account1.movements);
